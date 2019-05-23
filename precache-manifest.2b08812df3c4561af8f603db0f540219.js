4ef7f5e5ba219c47368864f49b6d440d1bd06441
William Stam <williamstam@gmail.com>
2019-05-23 19:10:53 +0100

    Updates

---
+++ precache-manifest.2b08812df3c4561af8f603db0f540219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c06e57ac5c4042e645d9544c6cbc17c",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "5043797b2324113fcea5",
+    "url": "/FaceRecognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b28707b828fe4d745186",
+    "url": "/FaceRecognition/static/css/main.7f2bf25c.chunk.css"
+  },
+  {
+    "revision": "5043797b2324113fcea5",
+    "url": "/FaceRecognition/static/js/2.0038b443.chunk.js"
+  },
+  {
+    "revision": "b28707b828fe4d745186",
+    "url": "/FaceRecognition/static/js/main.73beed78.chunk.js"
+  },
+  {
+    "revision": "c762eb56584dcbc05797",
+    "url": "/FaceRecognition/static/js/runtime~main.4997f496.js"
+  }
+]);
